54c4afa96e0355a397a79388f06406c460e2192d
Polash Ahmed <134755739+Polash-Ahmed@users.noreply.github.com>
2025-01-19 16:07:00 +0600

    Update index.js
    something added

--- index.js
+++ index.js
@@ -68,4 +68,19 @@ return [longestWord,names.indexOf(longestWord)]
 }
 console.log(longString(["polash ahmed","polash ahmudg","polash khan","poran mia","sojib islam"]));
 
+// ১ থেকে ১০০ পর্যন্ত কন সংখা গুলো ৩,৫ এবং ৩ ও ৫ উভয় সংখ্যা দ্বারা বিভাজ্য  তা বের করুন ।
 
+function fizzBuzz(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 15 === 0) {
+      console.log(`${i} is FizzBuzz`);
+    } else if (i % 3 === 0) {
+      console.log(`${i} is Fizz`);
+    } else if (i % 5 === 0) {
+      console.log(`${i} is Buzz`);
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(100)

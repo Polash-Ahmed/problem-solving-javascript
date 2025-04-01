07ac63d4ebf92c6dad864eccf7a9dde588c848ca
Polash Ahmed <134755739+Polash-Ahmed@users.noreply.github.com>
2025-04-01 10:59:06 +0600

    Update reduce.js
    avg age added

--- reduce.js
+++ reduce.js
@@ -111,3 +111,74 @@ bigArray.reduce((acc, cur) => {
   return acc;
 },[]);
 console.timeEnd('Reduce')
+
+
+// customer avg age
+
+let custormers = [
+  {
+    id: 1,
+    f_name: "abby",
+    l_name: "thomas",
+    gender: "M",
+    married: true,
+    age: 32,
+    expense: 500,
+    purchased: ["shampoo", "Toys", "Books"],
+  },
+  {
+    id: 2,
+    f_name: "jerry",
+    l_name: "tom",
+    gender: "M",
+    married: true,
+    age: 64,
+    expense: 100,
+    purchased: ["stick", "blade"],
+  },
+  {
+    id: 3,
+    f_name: "Dianna",
+    l_name: "Cherry",
+    gender: "F",
+    married: true,
+    age: 22,
+    expense: 1500,
+    purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+  },
+  {
+    id: 4,
+    f_name: "Dev",
+    l_name: "Currain",
+    gender: "M",
+    married: true,
+    age: 82,
+    expense: 90,
+    purchased: ["Book"],
+  },
+  {
+    id: 5,
+    f_name: "Maria",
+    l_name: "Gomes",
+    gender: "F",
+    married: false,
+    age: 7,
+    expense: 300,
+    purchased: ["Toys"],
+  },
+];
+
+let count = 0;
+const total = custormers.reduce((accumulator,customer,currentIndex,array)=>{
+  if(customer.purchased.includes('Book')){
+    accumulator = accumulator + customer.age;
+    count = count +1
+  }
+  return accumulator
+},0)
+
+
+const avgCustBookPurched = Math.floor(total / count)
+console.log(total);
+console.log(avgCustBookPurched);
+ 

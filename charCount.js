e95ec59c28fa326c60b3c5f938725bf88a8851b9
Polash Ahmed <134755739+Polash-Ahmed@users.noreply.github.com>
2026-04-21 23:30:47 +0600

    Create charCount.js

---
+++ charCount.js
@@ -0,0 +1,16 @@
+function charCount(str) {
+  let charList = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (/[a-z 0-9]/.test(char)) {
+      if (charList[char] > 0) {
+        charList[char]++;
+      } else {
+        charList[char] = 1;
+      }
+    }
+  }
+  return charList;
+}
+
+console.log(charCount('hello everyone !!'));

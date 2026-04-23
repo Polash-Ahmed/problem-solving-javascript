22c216066a58f9c38edb00e0b6494ef44833cd1e
Polash Ahmed <134755739+Polash-Ahmed@users.noreply.github.com>
2026-04-23 22:32:42 +0600

    Update recurson.js

--- recurson.js
+++ recurson.js
@@ -16,3 +16,21 @@ function helperSum(arr, index) {
 }
 
 console.log(sumOfArray([2, 3, 5]));
+
+
+// odd number solving
+
+function findOddNumber(arr) {
+  let result = [];
+  function helper(input) {
+    if (input.length === 0) return;
+
+    if (input[0] % 2 !== 0) {
+      result.push(input[0]);
+    }
+    helper(input.slice(1));
+  }
+  helper(arr);
+  return result;
+}
+console.log(findOddNumber([2, 3, 5, 7, 3, 4, 12, 32, 55, 75]));

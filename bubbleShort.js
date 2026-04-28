bee1868f856f36a206a17768c072e8ef105fb7f4
Polash Ahmed <134755739+Polash-Ahmed@users.noreply.github.com>
2026-04-28 23:02:54 +0600

    Update bubbleShort.js

--- bubbleShort.js
+++ bubbleShort.js
@@ -1,6 +1,6 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length -i - 1; j++) {
       // console.log("to see",arr,arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
